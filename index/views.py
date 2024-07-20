1e5841d89e4bf62eb183793ee158cb30baa8c40a
v.sanaidis <vsanaidis@acg.edu>
2024-07-20 23:55:46 +0300

    Updated views.py

--- index/views.py
+++ index/views.py
@@ -5,17 +5,3 @@ from django.contrib import messages
 def index_view(request):
     return render(request, 'index.html')
 
-
-def user_search(request):
-    query = request.GET.get('q')
-    users = User.objects.all()  
-    
-    if query:
-        users = users.filter(username__icontains=query)  
-    
-    context = {
-        'users': users,
-        'query': query,
-    }
-    
-    return render(request, 'user_search.html', context)
