462c0a352879a9366f9a7ce84e173f1fbe7d0a44
v.sanaidis <vsanaidis@acg.edu>
2024-07-21 22:52:21 +0300

    fix on wsgi.py

--- final/wsgi.py
+++ final/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for your_project_name project.
+WSGI config for final project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'final.settings')
 
 application = get_wsgi_application()
