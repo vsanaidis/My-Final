acf01a4f472f9ead73c83f49f3aa8181be57a290
v.sanaidis <vsanaidis@acg.edu>
2024-07-21 16:14:24 +0300

    Debug=false

--- final/settings.py
+++ final/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-cx%7ijeaf$adbfbdh4ye^pkj(yhw_b*1$5+z+$=o#lx3hehi@m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['dereeplugged.azurewebsites.net']
 
 
 # Application definition
