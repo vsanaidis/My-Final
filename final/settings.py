b25b773e7b6b3db899a6f0b2a8b81cee339b5901
v.sanaidis <vsanaidis@acg.edu>
2024-07-21 10:50:33 +0300

    fixes

--- final/settings.py
+++ final/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_URL = '/static/'
+STATIC_URL = '../static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
