7434d151fe4a037dacba6ed45dd4f0bd34644b8b
v.sanaidis <vsanaidis@acg.edu>
2024-07-22 01:56:22 +0300

    fixes

--- final/settings.py
+++ final/settings.py
@@ -145,12 +145,9 @@ LOGIN_URL = '/login/'
 # Azure Storage Settings
 AZURE_ACCOUNT_NAME = os.environ.get('AZURE_ACCOUNT_NAME')
 AZURE_ACCOUNT_KEY = os.environ.get('AZURE_ACCOUNT_KEY')
-AZURE_CONTAINER = 'staticplugged'  # Replace with your actual container name
+AZURE_CONTAINER = 'static'
 
-# Azure Custom Domain and Static URL
 AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
 STATIC_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{AZURE_CONTAINER}/'
 
-# Storage backends
 STATICFILES_STORAGE = 'storages.backends.azure_storage.AzureStorage'
-DEFAULT_FILE_STORAGE = 'storages.backends.azure_storage.AzureStorage'
